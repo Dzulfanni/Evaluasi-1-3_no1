85232c0f2a058dd89af478d8fe2e62b92198347b
Dzulfanni <belajarayok866@gmail.com>
2020-09-26 13:32:51 +0700

    first commit

---
+++ no1.js
@@ -0,0 +1,12 @@
+function mentorProgrammer(nama,status,asal){
+    nama();
+    status();
+    asal();
+} function nama(){
+    console.log("Nama Mentor Wandi Pratama")
+}function status(){
+    console.log("Staus nya Santri")
+}function asal(){
+    console.log("Berasal dari kota Prabumulih")
+}
+mentorProgrammer(nama,status,asal);
